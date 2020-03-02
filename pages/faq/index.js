3596335424c7b5a8ed07e63ea45216bef03595db
Gregg <wehmenstein@gmail.com>
2020-03-01 21:47:38 -0600

    tweak: one more typo

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -14,7 +14,7 @@ const FAQ = () => (
         <div className="card">
           <h3 className="card__question bold">When are RSVPs due?</h3>
           <p className="card__answer">
-            Plase RSVP as soon as you can confirm your attendance and no later than
+            Please RSVP as soon as you can confirm your attendance and no later than
             March 25th, 2020.
             </p>
         </div>
