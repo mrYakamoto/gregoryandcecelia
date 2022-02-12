5a2b7f8dcec22632f9228a1dc1313112f4620117
Gregg <gwehmeier@atlassian.com>
2022-02-11 20:29:33 -0600

    update: add cecelias copy updates

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -14,9 +14,11 @@ const FAQ = () => (
         <div className="card">
           <h3 className="card__question bold">When are RSVPs due?</h3>
           <p className="card__answer">
+            Even if you previously RSVP’d, please send us an updated response for our
+            2022 date, so we can be sure of your plans.
             Please RSVP as soon as you can confirm your attendance and no later than
-            March 09th, 2022.
-            </p>
+            February 25th, 2022.
+          </p>
         </div>
         <div className="card">
           <h3 className="card__question bold">Do guests need a COVID-19 vaccination?</h3>
@@ -41,8 +43,9 @@ const FAQ = () => (
             completely outdoors, and if luck is on our side we will be celebrating amidst
             a big wildflower meadow.
 
-            This means we recommend low, wedge, or no heels. We also recommend bringing
-            layers, as an April night can get chilly.
+            YES - it will be completely outdoors and on a lawn. This means we recommend low,
+            wedge, or no heels. We also recommend bringing layers, as an April night could
+            get chilly.
             </p>
         </div>
         <div className="card">
@@ -50,8 +53,8 @@ const FAQ = () => (
           <p className="card__answer">
             Austin can be incredibly unpredictable in the Spring, but we're hoping it will
             be nice! Here's what the average are for April 25th in Austin:
-            Average High: 80
-            Average Low: 59
+            Average High: 76
+            Average Low: 60
 
             We recommend bringing a jacket for the evening just in case.
             </p>
@@ -63,15 +66,20 @@ const FAQ = () => (
             use ride shares (Lyft or Uber) to get to the event. This will cut down the
             number of cars on the street and also enable you to have a fun, carefree time.
 
-            If you are staying with, or near, others you can make sure group travelling.
+            If you are staying around Downtown, please let us know if you'd like a ride in
+            our shuttle bus. It will be transporting people from the Hilton Downtown.
             Please reach out to us if you need help or want to carpool!
             </p>
         </div>
         <div className="card">
           <h3 className="card__question bold">Will transportation be provided to the ceremony and/or reception?</h3>
           <p className="card__answer">
-            Not specifically. Please use ride shares and our Lyft promo code to get to the event.
-            </p>
+            Yes - we have a shuttle bus leaving from the Hilton Downtown and transporting
+            people back again at the end of the night. It’s large, but let us know if you
+            are interested so we can get an accurate headcount of who would like a ride.
+
+            Otherwise, please plan on Rideshares.
+          </p>
         </div>
         <div className="card">
           <h3 className="card__question bold">What if I have dietary restrictions?</h3>
@@ -94,8 +102,8 @@ const FAQ = () => (
         <div className="card">
           <h3 className="card__question bold">Do you have any hotel room blocks?</h3>
           <p className="card__answer">
-            No, we don't have any rooms blocked at hotels. We recommend looking at hotels in
-            Downtown Austin.
+            No, we don't have any rooms blocked at hotels, but we may be able to help
+            you find a spot. We recommend looking at hotels in Downtown Austin.
 
             We are also recommending Airbnb as an option. We would be happy to let you know
             who's coming so that you can organize a shared place with someone you know!
@@ -104,10 +112,10 @@ const FAQ = () => (
         <div className="card">
           <h3 className="card__question bold">Can I bring my Children?</h3>
           <p className="card__answer">
-            In order to allow all guests, including parents, an evening of relaxation we have
-            only invited family members and very young children or infants. We hope this advance
-            notice means you are still able to share our big day and will enjoy having the
-            evening off!
+            We have only invited kids from our immediate family (3 of them). In order to allow
+            all guests, including parents, an evening of relaxation we have only invited family
+            members and very young children or infants. We hope this advance notice means you
+            are still able to share our big day and will enjoy having the evening off!
           </p>
         </div>
       </section>
